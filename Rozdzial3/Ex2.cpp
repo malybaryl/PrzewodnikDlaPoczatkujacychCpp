43a49c38ba0e7eee0d75cb418dbac6e7efe46ec9
Kacper Baryłowicz <112568080+malybaryl@users.noreply.github.com>
2023-04-17 01:36:10 +0200

    Add files via upload

---
+++ Rozdzial3/Ex2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double FirstNumber;
+	double SecondNumber;
+	
+	cout << "SUM TWO NUMBERS" << endl
+		<< "Enter first number: ";
+	cin >> FirstNumber;
+	cout << "Enter second number: ";
+	cin >> SecondNumber;
+	cout << "SUM: " << FirstNumber + SecondNumber;
+}
---
+++ Rozdzial3/Ex3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double FirstNumber;
+	double SecondNumber;
+
+	cout << "DIVIDE TWO NUMBERS" << endl
+		<< "Enter first number: ";
+	cin >> FirstNumber;
+	cout << "Enter second number: ";
+	cin >> SecondNumber;
+	cout << "QUOTIENT: " << FirstNumber / SecondNumber;
+}
