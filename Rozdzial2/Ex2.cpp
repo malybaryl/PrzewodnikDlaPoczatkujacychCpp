ded3849374d58dda9606181d1f79caf783658cab
Kacper Baryłowicz <112568080+malybaryl@users.noreply.github.com>
2023-04-16 21:44:13 +0200

    Add files via upload

---
+++ Rozdzial2/Ex2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "My friends' names; " << endl
+		<< "1. Oliwia <3" << endl
+		<< "2. Kacper" << endl
+		<< "3. Adrianna" << endl
+		<< "4. Julia" << endl
+		<< "5. Mateusz";
+}
