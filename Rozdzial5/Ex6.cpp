83f27b11182cd904e6371044f66540ed75565ae6
Kacper Baryłowicz <112568080+malybaryl@users.noreply.github.com>
2023-04-17 17:27:28 +0200

    Add files via upload

---
+++ Rozdzial5/Ex6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "SQUARE NUMBERS" << endl << endl;
+	for (int i = 0; i <= 20; i++)
+	{
+		cout << i << " : " << i * i << endl;
+	}
+}
