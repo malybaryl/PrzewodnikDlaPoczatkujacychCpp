265260ee4f06e6bcbc3f63d19ce5fad94e61cc6c
Kacper Baryłowicz <112568080+malybaryl@users.noreply.github.com>
2023-04-18 19:09:31 +0200

    Add files via upload

---
+++ Rozdzial7/Ex2.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 0;
+	
+	switch (x)
+	{
+	case 0:
+		cout << "On the first day of Christmas, my true love sent to me" << endl
+			<< "A partridge in a pear tree" << endl << endl;
+	case 1:
+		cout << "On the second day of Christmas, my true love sent to me" << endl
+			<< "Two turtledoves" << endl
+			<< "And a partridge in a pear tree" << endl << endl;
+	case 2:
+		cout << "On the third day of Christmas, my true love sent to me" << endl
+			<< "Three French hens" << endl
+			<< "Two turtledoves" << endl
+			<< "And a partridge in a pear tree" << endl << endl;
+	case 3:
+		cout << "On the fourth day of Christmas, my true love sent to me" << endl
+			<< "Four calling birds" << endl
+			<< "Three French hens" << endl
+			<< "Two turtledoves" << endl
+			<< "And a partridge in a pear tree" << endl << endl;
+	case 4:
+		cout << "On the fifth day of Christmas, my true love sent to me" << endl
+			<< "Five gold rings(five golden rings)" << endl
+			<< "Four calling birds" << endl
+			<< "Three French hens" << endl
+			<< "Two turtledoves" << endl
+			<< "And a partridge in a pear tree" << endl << endl;
+	case 5:
+		cout << "On the sixth day of Christmas, my true love sent to me" << endl
+			<< "Six geese a - laying" << endl
+			<< "Five gold rings(five golden rings)" << endl
+			<< "Four calling birds" << endl
+			<< "Three French hens" << endl
+			<< "Two turtledoves" << endl
+			<< "And a partridge in a pear tree" << endl << endl;
+	case 6:
+		cout << "On the seventh day of Christmas, my true love sent to me" << endl
+			<< "Seven swans a - swimming" << endl
+			<< "Six geese a - laying" << endl
+			<< "Five gold rings(five golden rings)" << endl
+			<< "Four calling birds" << endl
+			<< "Three French hens" << endl
+			<< "Two turtledoves" << endl
+			<< "And a partridge in a pear tree" << endl << endl;
+	case 7:
+		cout << "On the eighth day of Christmas, my true love sent to me" << endl
+			<< "Eight maids a - milking" << endl
+			<< "Seven swans a - swimming" << endl
+			<< "Six geese a - laying" << endl
+			<< "Five gold rings(five golden rings)" << endl
+			<< "Four calling birds" << endl
+			<< "Three French hens" << endl
+			<< "Two turtledoves" << endl
+			<< "And a partridge in a pear tree" << endl << endl;
+	case 8:
+		cout << "On the ninth day of Christmas, my true love sent to me" << endl
+			<< "Nine ladies dancing" << endl
+			<< "Eight maids a - milking" << endl
+			<< "Seven swans a - swimming" << endl
+			<< "Six geese a - laying" << endl
+			<< "Five gold rings(five golden rings)" << endl
+			<< "Four calling birds" << endl
+			<< "Three French hens" << endl
+			<< "Two turtledoves" << endl
+			<< "And a partridge in a pear tree" << endl << endl;
+	case 9:
+		cout << "On the tenth day of Christmas, my true love sent to me" << endl
+			<< "Ten lords a - leaping" << endl
+			<< "Nine ladies dancing" << endl
+			<< "Eight maids a - milking" << endl
+			<< "Seven swans a - swimming" << endl
+			<< "Six geese a - laying" << endl
+			<< "Five gold rings(five golden rings)" << endl
+			<< "Four calling birds" << endl
+			<< "Three French hens" << endl
+			<< "Two turtledoves" << endl
+			<< "And a partridge in a pear tree" << endl << endl;
+	case 10:
+		cout << "On the eleventh day of Christmas, my true love sent to me" << endl
+			<< "I sent eleven pipers piping" << endl
+			<< "Ten lords a - leaping" << endl
+			<< "Nine ladies dancing" << endl
+			<< "Eight maids a - milking" << endl
+			<< "Seven swans a - swimming" << endl
+			<< "Six geese a - laying" << endl
+			<< "Five gold rings(five golden rings)" << endl
+			<< "Four calling birds" << endl
+			<< "Three French hens" << endl
+			<< "Two turtledoves" << endl
+			<< "And a partridge in a pear tree" << endl << endl;
+	case 11:
+		cout << "On the twelfth day of Christmas, my true love sent to me" << endl
+			<< "Twelve drummers drumming" << endl
+			<< "Eleven pipers piping" << endl
+			<< "Ten lords a - leaping" << endl
+			<< "Nine ladies dancing" << endl
+			<< "Eight maids a - milking" << endl
+			<< "Seven swans a - swimming" << endl
+			<< "Six geese a - laying" << endl
+			<< "Five gold rings(five golden rings)" << endl
+			<< "Four calling birds" << endl
+			<< "Three French hens" << endl
+			<< "Two turtledoves" << endl
+			<< "And a partridge in a pear tree" << endl << endl;
+	case 12:
+		cout << "And a partridge in a pear tree" << endl;
+
+	default:
+		break;
+	}
+	
+	return 0;
+}
