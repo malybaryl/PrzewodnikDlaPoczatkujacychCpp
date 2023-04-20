c44190d4f7d4396c042f237064134a3575c4a1bf
Kacper Baryłowicz <112568080+malybaryl@users.noreply.github.com>
2023-04-20 23:58:58 +0200

    Add files via upload

---
+++ Rozdzial8/Ex1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int chance;
+	chance = rand() % 101;
+	if (chance <= 50)
+	{
+		cout << "ORZEL";
+	}
+	else
+	{
+		cout << "RESZKA";
+	}
+}
