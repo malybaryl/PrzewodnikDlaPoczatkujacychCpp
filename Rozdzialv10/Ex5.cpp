fae891d1cdb676fff6ac95f3a64263957bceabba
Kacper Baryłowicz <112568080+malybaryl@users.noreply.github.com>
2023-10-05 21:56:08 +0200

    Add files via upload

--- Rozdzialv10/Ex5.cpp
+++ Rozdzialv10/Ex5.cpp
@@ -30,13 +30,48 @@ void startGame(char Board[50][50],int& numberOfFields)
 
 void checkIfWin(char Board[50][50],int numberOfFields,bool& gameIsOn)
 {
- for (int i=0; i<numberOfFields ; i++)
+ for (int i=3; i<numberOfFields ; i++)
  {
     for (int j=0;i<numberOfFields;i++)
     {
-        if(Board[i][j]=='X'&&Board[i+1][j]=='X'&&Board[i+2][j]=='X'&&Board[i+3][j]=='X')
+        if(Board[i][j]=='X'&&Board[i-1][j]=='X'&&Board[i-2][j]=='X'&&Board[i-3][j]=='X')
         {
             std::cout << "X has won" << std::endl;
+            gameIsOn=false;
+        }
+        else if(Board[i][j]=='X'&&Board[i][j+1]=='X'&&Board[i][j+2]=='X'&&Board[i][j+3]=='X')
+        {
+            std::cout << "X has won" << std::endl;
+            gameIsOn=false;
+        }
+        else if(Board[i][j]=='X'&&Board[i-1][j-1]=='X'&&Board[i-2][j-2]=='X'&&Board[i-3][j-3]=='X')
+        {
+            std::cout << "X has won" << std::endl;
+            gameIsOn=false;
+        }
+        else if(Board[i][j]=='X'&&Board[i-1][j+1]=='X'&&Board[i-2][j+2]=='X'&&Board[i-3][j+3]=='X')
+        {
+            std::cout << "X has won" << std::endl;
+            gameIsOn=false;
+        }
+        else if(Board[i][j]=='O'&&Board[i-1][j]=='O'&&Board[i-2][j]=='O'&&Board[i-3][j]=='O')
+        {
+            std::cout << "O has won" << std::endl;
+            gameIsOn=false;
+        }
+        else if(Board[i][j]=='O'&&Board[i][j+1]=='O'&&Board[i][j+2]=='O'&&Board[i][j+3]=='O')
+        {
+            std::cout << "O has won" << std::endl;
+            gameIsOn=false;
+        }
+        else if(Board[i][j]=='O'&&Board[i-1][j-1]=='O'&&Board[i-2][j-2]=='O'&&Board[i-3][j-3]=='O')
+        {
+            std::cout << "O has won" << std::endl;
+            gameIsOn=false;
+        }
+        else if(Board[i][j]=='O'&&Board[i-1][j+1]=='O'&&Board[i-2][j+2]=='O'&&Board[i-3][j+3]=='O')
+        {
+            std::cout << "O has won" << std::endl;
             gameIsOn=false;
         }
     }
@@ -97,49 +132,108 @@ y = move%numberOfFields;
 
 int main()
 {
-	bool X;
-    bool O;
-    bool correct;
-    bool gameIsOn=true;
-    int move;
-	char Board[50][50];
-    std::vector<int> moves;
-    int numberOfFields = 0;
-	startGame(Board,numberOfFields);
-	int area;
-	while (gameIsOn==true)
-	{
-		checkIfWin(Board,numberOfFields,gameIsOn);
-        X=false;
-        O=false;
-        correct = false;
-        showBoard(Board,numberOfFields);
-        while(correct == false)
+    bool goOn=true;
+	while(goOn == true)
+    {
+        bool X;
+        bool O;
+        bool correct;
+        bool gameIsOn=true;
+        char decision=' ';
+        int move;
+        char Board[50][50];
+        std::vector<int> moves;
+        int numberOfFields = 0;
+        startGame(Board,numberOfFields);
+        int area;
+	    while (gameIsOn==true)
         {
-            int x = 0;
-            int y = 0;
-            std::cout << "X:";
-            std::cin >> move;
-            transformMove(move, numberOfFields,x,y);
-            X=true;
-            markField(Board,moves,numberOfFields,X,O,move,correct,x,y);
-            showBoard(Board,numberOfFields);
+            checkIfWin(Board,numberOfFields,gameIsOn);
+            if(gameIsOn==false)
+            {
+                break;
+            }
+            if(moves.size()==numberOfFields*numberOfFields)
+            {
+                std::cout << "DRAW" << std::endl;
+            }
             X=false;
-        }
-        correct = false;
-        checkIfWin(Board,numberOfFields,gameIsOn);
-        while(correct == false)
-        {
-            int x = 0;
-            int y = 0;
-            std::cout << "O:";
-            std::cin >> move;
-            transformMove(move, numberOfFields,x,y);
-            O=true;
-            markField(Board,moves,numberOfFields,X,O,move,correct,x,y);
-            showBoard(Board,numberOfFields);
             O=false;
+            correct = false;
+            std::cout << std::endl;
+            showBoard(Board,numberOfFields);
+            while(correct == false)
+            {
+                int x = 0;
+                int y = 0;
+                std::cout << "X:";
+                std::cin >> move;
+                if(numberOfFields > 0 && move <= numberOfFields*numberOfFields)
+                {
+                    transformMove(move, numberOfFields,x,y);
+                    X=true;
+                    markField(Board,moves,numberOfFields,X,O,move,correct,x,y);
+                    std::cout << std::endl;
+                    showBoard(Board,numberOfFields);
+                    X=false;    
+                }
+                else
+                {
+                    std::cout << "wrong range..." << std::endl;
+                }
+
+            }
+            correct = false;
+            checkIfWin(Board,numberOfFields,gameIsOn);
+            if(gameIsOn==false)
+            {
+                break;
+            }
+            if(moves.size()==numberOfFields*numberOfFields)
+            {
+                std::cout << "DRAW" << std::endl;
+                break;
+            }
+            while(correct == false)
+            {
+                int x = 0;
+                int y = 0;
+                std::cout << "O:";
+                std::cin >> move;
+                if(numberOfFields > 0 && move <= numberOfFields*numberOfFields)
+                {
+                    transformMove(move, numberOfFields,x,y);
+                    O=true;
+                    markField(Board,moves,numberOfFields,X,O,move,correct,x,y);
+                    std::cout << std::endl;
+                    showBoard(Board,numberOfFields);
+                    O=false;
+                }
+                else
+                {
+                    std::cout << "wrong range..." << std::endl;
+                }
+            }
         }
-	}
-	return 0;
+        while (true)
+        {
+        std::cout << "Do you want to play again? y/n: ";
+        std::cin >> decision;
+        if(decision == 'n')
+            {
+                goOn = false;
+                break;
+            }
+            else if(decision == 'y')
+            {
+                break;
+            }
+            else if (decision != 'y')
+            {
+            std::cout << "wrong commend..." << std::endl;
+            break;
+            }
+        }
+    }
+    return 0;
 }
